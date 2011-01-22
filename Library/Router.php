0bd05125e6a63f4dc1012baf4aa9e8d08235cfbb
SuprDewd <suprdewd@gmail.com>
2011-01-22 14:10:03 +0000

    Now checks for action visibility.

--- Library/Router.php
+++ Library/Router.php
@@ -11,20 +11,25 @@ class Router
 		$action = count($urlArray) > 0 ? $urlArray[0] : Config::Read('Router.Default.Action');
 		array_shift($urlArray);
 		
-		if (__autoload($controller))
-		{
-			$dispatch = new $controller(/*$model, $controllerName, $action*/);
-			
-			if ($action[0] !== '_' && method_exists($controller, $action)/* && TODO: Action must be a public function! */)
-			{
-				if (method_exists($controller, 'BeforeAction')) call_user_func_array(array($dispatch, 'BeforeAction'), $urlArray);
-				call_user_func_array(array($dispatch, $action), $urlArray);
-				if (method_exists($controller, 'AfterAction')) call_user_func_array(array($dispatch, 'AfterAction'), $urlArray);
-			}
-			else if ($isFallback) return; // TODO: Handle Error! 
-			else self::Initialize('Http/Error404', true);
-		}
-		else if ($isFallback) return; // TODO: Handle Error!
+		if (!__autoload($controller)) { self::Error(); return; }
+		
+		$dispatch = new $controller();
+		
+		if (!($action[0] !== '_' && $action !== 'BeforeAction' && $action !== 'AfterAction' && method_exists($controller, $action))) { self::Error(); return; }
+		
+		$dispatchAction = new ReflectionMethod($dispatch, $action);
+		
+		if (!$dispatchAction->isPublic() || $dispatchAction->isStatic()) { self::Error(); return; }
+		
+		if (method_exists($controller, 'BeforeAction')) call_user_func_array(array($dispatch, 'BeforeAction'), $urlArray);
+		// call_user_func_array(array($dispatch, $action), $urlArray);
+		$dispatchAction->invokeArgs($dispatch, $urlArray);
+		if (method_exists($controller, 'AfterAction')) call_user_func_array(array($dispatch, 'AfterAction'), $urlArray);
+	}
+
+	private static function Error($isFallback)
+	{
+		if ($isFallback) return; // TODO: Handle Error!
 		else self::Initialize('Http/Error404', true);
 	}
 }
